8d6877487b31bae019855d94031c0c0be266a803
KévinLePD <group.g7.seven@gmail.com>
2015-11-20 15:15:10 +0100

    infos,menu

--- view/infos.php
+++ view/infos.php
@@ -1,10 +1,10 @@
 <div class="col-lg-6">
           <h2>Dernières infos</h2>
           <section class="row">
-            <div class="col-lg-3 col-sm-5 col-md-5"><img src="images/1.jpg"/></div>
-            <div class="col-lg-3 col-sm-5 col-md-5"><img src="images/2.jpg"/></div>
-            <div class="col-lg-3 col-sm-5 col-md-5"><img src="images/3.jpg"/></div>
-            <div class="col-lg-3 col-sm-5 col-md-5"><img src="images/4.jpg"/></div>
-            <div class="col-lg-3 col-sm-5 col-md-5"><img src="images/5.jpg"/></div>
+            <div class="col-lg-5 col-sm-5 col-md-5"><img src="images/1.jpg"/></div>
+            <div class="col-lg-5 col-sm-5 col-md-5"><img src="images/2.jpg"/></div>
+            <div class="col-lg-5 col-sm-5 col-md-5"><img src="images/3.jpg"/></div>
+            <div class="col-lg-5 col-sm-5 col-md-5"><img src="images/4.jpg"/></div>
+            <div class="col-lg-5 col-sm-5 col-md-5"><img src="images/5.jpg"/></div>
           </section>
         </div>
--- view/menu.php
+++ view/menu.php
@@ -1,14 +1,9 @@
-<body>
-    <div class="container">
-      <header class="page-header">
-        <h1>Booking Seven</h1>
-      </header>
-      <section class="row">
-        <div class="col-lg-3" id="Menu">
-          <h2>Menu</h2>
-          <ul>
-            <li><a href="#">Gestion profil</a></li>
-            <li><a href="#">Gerer planing</a></li>
-            <li><a href="#">Gerer groupes</a></li>
-          </ul>
-        </div>
+
+<div class="col-lg-3" id="Menu">
+  <h2>Menu</h2>
+  <ul>
+    <li><a href="#">Gestion profil</a></li>
+    <li><a href="#">Gerer planing</a></li>
+    <li><a href="#">Gerer groupes</a></li>
+  </ul>
+</div>
